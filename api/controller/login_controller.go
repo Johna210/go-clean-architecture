2f6efb8556adfa4c894b4d2a5851fd0c2237db05
Johna210 <solomonjohna21@gmail.com>
2024-08-10 22:30:15 +0300

    Implemented controller

---
+++ api/controller/login_controller.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/Johna210/go-clean-architecture/bootstrap"
+	"github.com/Johna210/go-clean-architecture/domain"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type LoginController struct {
+	LoginUsecase domain.LoginUsecase
+	Env          *bootstrap.Env
+}
+
+func (lc *LoginController) Login(c *gin.Context) {
+	var request domain.LoginRequest
+
+	err := c.ShouldBind(&request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: err.Error()})
+	}
+
+	user, err := lc.LoginUsecase.GetUserByEmail(c, request.Email)
+	if err != nil {
+		c.JSON(http.StatusNotFound, domain.ErrorResponse{Message: "User not fiund with the given email"})
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)) != nil {
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials"})
+		return
+	}
+
+	accessToken, err := lc.LoginUsecase.CreateAccessToken(&user, lc.Env.AccessTokenSecret, lc.Env.AccessTokenExpiryHour)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	refreshToken, err := lc.LoginUsecase.CreateRefreshToken(&user, lc.Env.RefreshTokenSecret, lc.Env.RefreshTokenExpiryHour)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	loginResponse := domain.LoginResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}
+
+	c.JSON(http.StatusOK, loginResponse)
+}
---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+
+	"github.com/Johna210/go-clean-architecture/bootstrap"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := bootstrap.App()
+
+	env := app.Env
+
+	db := app.Mongo.Database(env.DBName)
+	defer app.CloseDBConnection()
+
+	timeout := time.Duration(env.ContextTimeout) * time.Second
+
+	gin := gin.Default()
+
+}
