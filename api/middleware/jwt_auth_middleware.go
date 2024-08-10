27640d444943a0ab4f995c4ee0bc24212b3ca2b6
Johna210 <solomonjohna21@gmail.com>
2024-08-10 11:02:22 +0300

    Implemented domain layer

---
+++ api/middleware/jwt_auth_middleware.go
@@ -0,0 +1,12 @@
+package middleware
+
+// func jwtAuthMiddleware(secret string) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		authHeader := c.Request.Header.Get("Authorization")
+// 		t := strings.Split(authHeader, " ")
+// 		if len(t) == 2 {
+// 			authToken := t[1]
+// 			authorized, err := toke
+// 		}
+// 	}
+// }
---
+++ internal/tokenUtil/token_util.go
@@ -0,0 +1,3 @@
+package tokenutil
+
+func CreateAccessToken()
